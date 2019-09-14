bea13f7386f98ad43d98411057789851a0ede4eb
Crawl_up <Ruffian@email.com>
2019-09-14 10:50:48 +0000

    5.5.3 完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,12 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  
+  private
+  
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

158d466c8c1dc497c755cae7a2dc9d4c01ef6403
Crawl_up <Ruffian@email.com>
2019-09-16 08:36:58 +0000

    8.1.2 完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,16 @@ class UsersController < ApplicationController
   end
   
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合の処理を記述します。
+    else
+      render :edit
+    end
+  end
+
+
   private
   
   

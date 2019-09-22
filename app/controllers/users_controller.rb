b55ae4b20a3b9c2952fe72582ad4319723086d60
Crawl_up <Ruffian@email.com>
2019-09-22 10:31:38 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,16 @@ class UsersController < ApplicationController
   
   
   def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      # 更新成功時の処理
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      # 更新失敗時の処理
+      flash[:danger] = "#{@user.name}の更新は失敗しました。 <br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
+
 
 
   private
@@ -69,6 +78,9 @@ class UsersController < ApplicationController
     end
     
     
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
     # beforeフィルター
     
     
